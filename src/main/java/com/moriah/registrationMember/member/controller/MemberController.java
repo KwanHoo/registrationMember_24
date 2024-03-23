eb417e4f7438eb7273587a8399397cc78b7c4f65
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-03-23 18:25:48 +0900

    Feat : 입력한 정보 컨트롤러로 전달하기

--- src/main/java/com/moriah/registrationMember/member/controller/MemberController.java
+++ src/main/java/com/moriah/registrationMember/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.moriah.registrationMember.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -10,5 +12,17 @@ public class MemberController {
     public String saveForm() {
         return "save"; // "save" 라는 이름의 파일을 templates 에서 찾음
     }
+
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+
+        System.out.println("MemberController.save"); //soutm
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName); //soutp
+        //memberEmail = aaa, memberPassword = aaaa, memberName = aaa1
+        return "index";
+    }
+
 
 }
