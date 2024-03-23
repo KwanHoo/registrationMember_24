223d4b667ecbd929d61238c1ee849f37574dedb1
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-03-23 17:47:35 +0900

    init project

---
+++ src/main/java/com/moriah/registrationMember/RegistrationMemberApplication.java
@@ -0,0 +1,13 @@
+package com.moriah.registrationMember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationMemberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moriah/registrationMember/RegistrationMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moriah.registrationMember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
