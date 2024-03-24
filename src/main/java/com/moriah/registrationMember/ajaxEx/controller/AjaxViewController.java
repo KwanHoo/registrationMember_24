ace88c0d38617520cc866c9f79b6b05dd7ef4084
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-03-24 12:46:04 +0900

    Feat : Ajax get 요청하기

---
+++ src/main/java/com/moriah/registrationMember/ajaxEx/controller/AjaxViewController.java
@@ -0,0 +1,12 @@
+package com.moriah.registrationMember.ajaxEx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AjaxViewController {
+    @GetMapping("/ajax-ex-01")
+    public String ajaxEx01(){
+        return "ajax-ex-01";
+    }
+}
