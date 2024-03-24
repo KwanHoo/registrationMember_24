c11889e83c2dc214b46bfe3d0c71446c2a312866
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-03-24 12:18:24 +0900

    Feat : 회원 로그아웃

--- src/main/java/com/moriah/registrationMember/member/controller/MemberController.java
+++ src/main/java/com/moriah/registrationMember/member/controller/MemberController.java
@@ -116,4 +116,11 @@ public class MemberController {
         return "redirect:/member/";
     }
 
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+
+        return "index";
+    }
+
 }
