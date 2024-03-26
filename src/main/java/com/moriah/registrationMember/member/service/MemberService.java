199d974b2b755538a2df6327fc91b72ad9e2727a
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-03-27 00:33:33 +0900

    Feat : Ajax 이메일 중복체크 구현 완료

--- src/main/java/com/moriah/registrationMember/member/service/MemberService.java
+++ src/main/java/com/moriah/registrationMember/member/service/MemberService.java
@@ -108,4 +108,16 @@ public class MemberService {
     public void deleteById(Long id) {
         memberRepository.deleteById(id);
     }
+
+    public String emailCheck(String memberEmail) {
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+
+        if(byMemberEmail.isPresent()) {
+            // 조회결과가 있다 -> 해당 입력값 사용할 수 없다.
+            return null;
+        } else {
+            // 조회결과가 없다 -> 해당 입력값 사용 가능!
+            return "ok";
+        }
+    }
 }
