6f7e8cf38f36a7b2b6cdbc6207cbe2a5aae999d6
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2024-03-26 21:35:43 +0900

    Feat : Ajax post 요청하기

--- src/main/java/com/moriah/registrationMember/ajaxEx/controller/AjaxController.java
+++ src/main/java/com/moriah/registrationMember/ajaxEx/controller/AjaxController.java
@@ -2,12 +2,28 @@ package com.moriah.registrationMember.ajaxEx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxController {
     @GetMapping("/ex01")
-    public String ex01() {
+    public String getAjaxex01() {
         System.out.println("AjaxController.ex01");
-        return "index";
+        return "index";     //index.html에 작성된 내용이 출력됨
+    }
+
+    @PostMapping("/ex02")
+    public @ResponseBody String ex02() {
+        System.out.println("AjaxController.ex02");
+        return "리턴값이 그대로 실려서 넘어간다";     //@ResponseBody 하면 리턴값이 그대로 출력됨
+    }
+
+    @GetMapping("/ex03")
+    public @ResponseBody String ex03(@RequestParam("param1") String param1, @RequestParam("param2") String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+
+        return "ex03 메서드 호출 완료";
     }
 }
